2ddf7a900da25be2214e18d489f3a0fa17c12ef7
Teddy Blanco <tblanco@student.42quebec.com>
2021-12-24 14:17:48 -0500

    :wrench: add first aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,1 +1,15 @@
+# aliases
+alias codezsh="code ~/.zshrc"
+alias reload="source ~/.zshrc"
 alias c="clear"
+
+# git
+alias gs="git status"
+alias ga!="git add --all && gs"
+
+function ga(){
+	git add $1 && git status
+}
+function gc(){
+	git commit -m "$1"
+}
